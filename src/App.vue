7d824d41dde237996f6e697e5a5780045cc987d1
Aaron Scofield <tuj83098@temple.edu>
2021-11-09 09:43:29 -0500

    data is loaded from db.json instead of hardcoding

--- src/App.vue
+++ src/App.vue
@@ -41,29 +41,15 @@ export default {
     }, 
     toggleReminder(id){
       this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+    },
+    async fetchTasks(){
+      const res = await fetch('http://127.0.0.1:5000/tasks');
+      const data = await res.json()
+      return data
     }
   },
-  created(){
-    this.tasks = [
-      {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'March 1st at 2:30pm',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'March 3rd at 1:30pm',
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'March 1st at 11:00am',
-        reminder: true,
-      },
-    ]
+  async created(){
+    this.tasks = await this.fetchTasks();
   }
 }
 </script>
